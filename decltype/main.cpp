20ce6e6ec153e919dbc2ded815d57f2d344709b0
jianglongjian <2511067577@qq.com>
2019-07-17 11:45:32 +0800

    Update(auto commit)

--- decltype/main.cpp
+++ decltype/main.cpp
@@ -14,7 +14,7 @@ using namespace std;
  decltype 用来进行给定表达式的类型推导，规则如下:
  1> exp是标识符，返回标识符的类型;
  2> exp是函数调用，返回函数返回值的类型;
- 3> 其他情况，当exp的结果类型可以作为左值，则返回对应类型的引用类型; 例如解引用操作。
+ 3> 其他情况，当exp可以作为左值，则返回对应类型的引用类型; 例如 `*`解引用操作, `()`操作符。
  */
 
 int main(int argc, const char * argv[]) {
@@ -23,6 +23,11 @@ int main(int argc, const char * argv[]) {
     decltype(i) a; // int
     decltype(r + 0) b; // r+0 => int
     decltype(*p) c; // int&, 必须初始化
+    decltype((i)) d; // int&
+    decltype((6)) e; // int
+
+    decltype(i++) f; // int
+    decltype(++i) g; // int&
 
     return 0;
 }
